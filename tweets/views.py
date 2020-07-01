a6ee8df6cb2f8034795eb8a9203c081b48039dfc
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-01 12:14:05 -0500

    Added JS to home starting dynamic build

--- tweets/views.py
+++ tweets/views.py
@@ -11,6 +11,16 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse()
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
