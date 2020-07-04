7dc28ea19f7b9c29f588a63bf63e2ac1db656105
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-03 19:16:40 -0500

    refactor tweet with function. Added 'likes' function and render

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -13,7 +14,8 @@ def home_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content,
+                    "likes": random.randint(0, 255)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
