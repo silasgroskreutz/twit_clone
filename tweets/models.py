7934c75852a511bc8e4b1244d2c019a2293567fc
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-05 08:48:16 -0500

    basic form functionality added. Able to add tweets

--- tweets/models.py
+++ tweets/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 
 class Tweet(models.Model):
-    content = models.TextField()
-    image = models.FileField(upload_to='images/')
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
