4681b02ba5c2da4d72e90aad785f15ed7fe52cb2
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-06 11:36:31 -0500

    Adding django created forms, and redirection

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,6 @@
 import random
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import TweetForm
 from .models import Tweet
@@ -15,9 +15,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url != None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
