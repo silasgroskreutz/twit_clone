fdcfbe7eb3456439f66566e114f7954c2d163550
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-09 07:37:29 -0500

    Making tweets Ajax request

--- tweets/views.py
+++ tweets/views.py
@@ -22,6 +22,9 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
+
         if next_url != None and is_safe_url(next_url):
             return redirect(next_url)
         form = TweetForm()
