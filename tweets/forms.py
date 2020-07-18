a8e8d9d1dc798c3e20a3e598bce9e6dcecc8dd51
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-07-18 16:09:22 -0500

    Changed views to use serializer

--- tweets/forms.py
+++ tweets/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 
 from .models import Tweet
--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
+from .serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Create your views here.
@@ -17,6 +18,14 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data=request.POST or None)
+    if serializer.is_valid():
+        obj = serializer.save(user=request.user)
+        return JsonResponse(serializer.data, status=201)
+    return JsonResponse({}, status=400)
+
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
     user = request.user
     if not.request.user.is_authenicated:
         if request.is_ajax():
